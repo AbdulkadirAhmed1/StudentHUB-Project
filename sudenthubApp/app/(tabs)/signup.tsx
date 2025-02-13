d0e38a20ec01de2fc4ded51c89106b4a22c67e73
Daksh Patel <dakshpatel1700@gmail.com>
2025-02-12 22:11:55 -0500

    Set Up UI for login page on signup.tsx

--- sudenthubApp/app/(tabs)/signup.tsx
+++ sudenthubApp/app/(tabs)/signup.tsx
@@ -1,11 +1,38 @@
 // calendar.tsx
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
-export default function AdvisingScreen() {
+export default function LoginScreen() {
+
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleLogin = () => {
+      if (username === 'admin' && password === 'admin') {
+         alert('Login successful');
+      } else {
+         alert('Login failed');
+      }
+   };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Redircting....</Text>
+      <Text style={styles.text}>Login</Text>
+      <TextInput
+          style={styles.input}
+          placeholder="Enter Username"
+          value={username}
+          onChangeText={setUsername}
+          />
+      <TextInput
+          style={styles.input}
+          placeholder="Enter Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -19,5 +46,27 @@ const styles = StyleSheet.create({
   },    
   text: {
     fontSize: 20,
+    color: 'black',
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    width: '90%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#f1f1f1',
+  },
+
+  button: {
+    backgroundColor: 'black',
+    width: '90%',
+    padding: 15,
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
